09fb71bcff7bb9392789ef24be7eb5337fe3dcb5
luisi <luisi@mamalon>
2026-08-23 14:33:28 -0400

    añadiendo clases

---
+++ proyecto/src/gestion_inmobilaria/Inmobiliaria.java
@@ -0,0 +1,8 @@
+
+package gestion_inmobilaria;
+
+
+public class Inmobiliaria{
+	private HashSet<AgenteInmobiliario> agentes = new HashSet<AgenteInmobiliario>();
+	private HashSet<ProyectoInmobiliario> proyectos = new HashSet<AgenteInmobiliario>();
+}
