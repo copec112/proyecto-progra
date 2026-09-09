d620289fd4b7945cf440cf13b254439c7989ccdb
Reveline <git@github.com>
2026-09-08 21:55:37 -0300

    Se agregaron los metodos para editar y eleminiar clientes

--- src/gestion_inmobilaria/AgenteInmobiliario.java
+++ src/gestion_inmobilaria/AgenteInmobiliario.java
@@ -8,6 +8,7 @@ package gestion_inmobilaria;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -61,7 +62,9 @@ public class AgenteInmobiliario {
 
     // Lógica de compra y venta 
 
-    
+    // TODO: procesarPago
+    public boolean venderPropiedad(Propiedad propiedad){}
+
     public void asignarDepartamento(Propiedad propiedad, Cliente cliente){
         if (!propiedad.setVendido()){
             cliente.agregarPropiedad(propiedad)
@@ -89,8 +92,61 @@ public class AgenteInmobiliario {
             System.out.println(i);
         }
     }
-    void editarCliente(String id){}
-    void eliminarCliente(String id){}
+
+    void editarCliente(String id){
+        Cliente clienteAux = mapaClientes.get(id);
+        Scanner buffer = new Scanner(System.in);
+        
+        System.out.println("Selecciona que que valor editar ");
+        System.out.println("1: ID ");
+        System.out.println("2: NOMBRE ");
+        System.out.println("Apretar cualquier otra tecla para cancelar ");
+        String opcion = buffer.nextLine();
+        
+        switch(expression){
+            case "1":
+                System.out.println("Escribe el nuevo ID: ");
+                clienteAux.setId(buffer.nextLine());
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+
+
+                break;
+            case "2":
+                System.out.println("Escribe el nuevo NOMBRE: ");
+                clienteAux.setNombre(buffer.nextLine());
+                
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+
+                break;
+            default:
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                break;
+        }
+
+    }
+
+    void eliminarCliente(String id){
+        if (!containsKey(id)){
+            System.out.println("El elemento con esa clave no existe.");
+            return;
+        }
+        Cliente clienteAux = mapaClientes.get(id);
+        System.out.println("¿Estas seguro que quieres eliminar " + cliente.getNombre() + "?");
+        System.out.println("S/n");
+
+        if (buffer.nextLine() == "S"){
+            clienteAux.remove(id);
+            return;
+        }
+
+        System.out.println("Operacion cancelada");
+
+    }
+
+    // TODO
     Cliente buscarCliente(String id){}
     Cliente buscarCliente(String nombre){}
 
